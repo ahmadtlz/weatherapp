8b0b8dd8b5166a6a8de56a4eaabfb47a6a558214
ahmad <ahhhmadtlz@gmail.com>
2020-11-28 17:13:09 +0330

    add some code to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Global from './Global.styled';
+import { getWeatherStart } from './store/modules/weather/weather.actions';
 
-const App :React.FC = () => (
-  <>
-    <Global />
-    <h1>wether app</h1>
-  </>
-);
+const App :React.FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getWeatherStart('tehran'));
+  }, [getWeatherStart]);
+
+  return (
+    <>
+      <Global />
+
+      <h1>wether app</h1>
+    </>
+  );
+};
 
 export default App;
