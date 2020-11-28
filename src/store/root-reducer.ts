68ca817541cfa5989693f272bdf0e6e1b0fa8a53
ahmad <ahhhmadtlz@gmail.com>
2020-11-28 17:13:22 +0330

    add  root reducer

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import alertReducer from './modules/alert/alert.reducer';
+import weatherReducer from './modules/weather/weather.reducer';
 
 const rootReducer = combineReducers({
-
+  weather: weatherReducer,
+  alert: alertReducer
 });
 
 export type RootState=ReturnType<typeof rootReducer>;
