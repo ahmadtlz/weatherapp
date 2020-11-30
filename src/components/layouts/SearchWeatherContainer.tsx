f4bc757c828abf82edbe42a4d97330057903305f
ahmad <ahhhmadtlz@gmail.com>
2020-11-30 09:55:01 +0330

    delete SearchWeather.styled  and add into the same file

--- src/components/layouts/SearchWeatherContainer.tsx
+++ src/components/layouts/SearchWeatherContainer.tsx
@@ -2,9 +2,19 @@ import { race } from 'redux-saga/effects';
 
 import React from 'react';
 
-import SearchWeatherContainerStyle from './SearchWeatherContainer.styled';
+import styled from 'styled-components';
 import SearchForm from '../organisms/SearchForm';
 import Title from '../atoms/Title/Title';
+
+const SearchWeatherContainerStyle = styled.div`
+  height:20em;
+  width:100vw;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  background-color:#efefef
+`;
 
 const SearchWeatherContainer:React.FC = () => (
   <SearchWeatherContainerStyle>
